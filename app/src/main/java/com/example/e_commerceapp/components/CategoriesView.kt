e142ecf27d8dc3c84a90d838c0b5e23539aae0c1
pradeeph45 <pradeephalemani8@gmail.com>
2025-12-13 21:47:25 +0530

    Displaying products with image and prices

--- app/src/main/java/com/example/e_commerceapp/components/CategoriesView.kt
+++ app/src/main/java/com/example/e_commerceapp/components/CategoriesView.kt
@@ -4,8 +4,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
@@ -13,7 +11,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
--- app/src/main/java/com/example/e_commerceapp/pages/CategoryProducts.kt
+++ app/src/main/java/com/example/e_commerceapp/pages/CategoryProducts.kt
@@ -1,10 +1,13 @@
 package com.example.e_commerceapp.pages
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
@@ -12,6 +15,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.e_commerceapp.Model.ProductModel
+import com.example.e_commerceapp.components.ProductItemView
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
 
@@ -38,8 +42,16 @@ fun CategoryProduct(modifier: Modifier = Modifier,categoryId : String){
         modifier = Modifier.fillMaxSize()
             .padding(16.dp)
     ) {
-        items(productsList.value) {item ->
-            Text(item.title)
+        items(productsList.value.chunked(2)) {rowItems ->
+            Row(modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(12.dp)) {
+                rowItems.forEach {
+                    ProductItemView(item = it, modifier = Modifier.weight(1f))
+                }
+                if(rowItems.size == 1){
+                    Spacer(modifier = Modifier.weight(1f))
+                }
+            }
         }
     }
 }
